2b932307042f1aec0dbe2bcdd482db593f5bc737
Gabriel Bhering <gbhering@protonmail.ch>
2019-07-12 04:19:15 -0300

    extra comments

--- sim.cc
+++ sim.cc
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <queue>
 #include <deque>
@@ -7,11 +8,11 @@ using namespace std;
 
 // Constantes do programa
 bool const FCFS = true;
-unsigned const RODADAS = 1000;
-float const KMIN = 100000;
+unsigned const RODADAS = 200;
+float const KMIN = 100;
 float const MI = 1.0;
 float const LAMBDA = 0.4;
-unsigned const VERBOSE = 0;
+unsigned const VERBOSE = 1;
 
 float W[RODADAS] = {};
 float N_q[RODADAS] = {};
@@ -49,56 +50,77 @@ void entra_servidor(float& w_i, float& k) {
 
 /*
 	Esta função é a que executa o loop de cada rodada. Até que KMIN coletas de 
+	tempos de espera sejam feitas, a rodada continua. 
 */
 void rodada() {
 	float k = 0, w_i = 0, N_qi = 0, t0 = T;
 	while (k <= KMIN and !fila.empty()) {
+		// Pirmeiro evento da fila é extraído
 		Evento e = fila.top();	fila.pop();
 		float delta = e.t - T;
+		// Tempo avança
 		T = e.t;
+		// Número de pessoas é computado
 		N_qi += espera.size()*delta;
+		// Caso o próximo evento seja uma chegada...
 		if ( e.tipo == chegada ) {
+			// Uma nova pessoa entra no sistema 
+			N++; 
+			// Próxima chegada é agendada
 			espera.push_back(e);
-			N++;
+			// Esta chegada é aramazenada
 			fila.emplace(chegada, T + exponencial(LAMBDA), R);
+			// Caso seja a única pessoa no sistema, 
 			if ( N == 1 ) 
+				// entra imediatemente no servidor
 				entra_servidor(w_i, k);
 		} 
+		// Caso o próximo evento seja uma partida...
 		else if ( e.tipo == partida ) {
+			// Uma pessoa sai do sistema
 			N--;
+			// Se ainda há pessoas no sistema, agenda uma nova partida
 			if ( N > 0 ) 
 				entra_servidor(w_i, k);
 		}
 	}
 
-	W[R] = w_i/KMIN; N_q[R] = N_qi/(T-t0);
+	// Guarda-se as estatisticas ao fim da rodada em vetores 
+	// O total de tempo esperado é dividido pelo número de pessoas
+	W[R] = w_i/KMIN; 
+	// O resultado da área do gráfico é dividido pelo tempo total 
+	N_q[R] = N_qi/(T-t0);
 	if (VERBOSE > 0) 
-		cout << "W"<<R<<"=" << W[R] << " Nq"<<R<<"=" << N_q[R] << endl;
+		cout << R<< '(' << W[R] << ", " << N_q[R] << ')' << endl;
 } 
 
 
 int main(void) {
-	srand(51520191);
 	cout << "rodadas=" << RODADAS 
 		 << " kmin=" << KMIN 
 		 << " lambda=" << LAMBDA 
 		 << endl;
 
+	// primeira chegada é agendada
 	fila.emplace(chegada, T + exponencial(LAMBDA), R);
 	while (R++ < RODADAS) 
 		rodada();
 
+	// ao fim das rodadas, calculamos as médias...
 	float ENq = 0, VNq = 0, EW = 0, VW = 0;
 	for (unsigned i = 0; i <= RODADAS; ++i) {
 		ENq += N_q[i]/RODADAS;
 		EW += W[i]/RODADAS;
 	}
+	// ...depois as variancias
 	for (unsigned i = 0; i <= RODADAS; ++i) {
-		VNq += ( (N_q[i]-ENq) * (N_q[i]-ENq) ) / ( RODADAS-1 );
-		VW += ( (W[i]-EW) * (W[i]-EW) ) / ( RODADAS-1 );
+		VNq += pow( (N_q[i]-ENq), 2 ) / ( RODADAS-1 );
+		VW += pow( W[i]-EW, 2 ) / ( RODADAS-1 );
 	}
 	cout << "E[W]=" << EW << " E[Nq]=" << ENq << endl;
 	cout << "Var[W]=" << VW << " Var[Nq]=" << VNq << endl;
+
+	cout << "Ultimo estado do gerador: " << gen << endl;
 
 	return 0;
 }
