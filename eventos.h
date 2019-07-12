b1f4a4f3e5eadb2a1e572a3f73e1d39a2f110071
Gabriel Bhering <gbhering@protonmail.ch>
2019-07-12 00:35:22 -0300

    now a little more nifty and with fun little tricks!

--- eventos.h
+++ eventos.h
@@ -1,5 +1,11 @@
-enum Tipo { chegada, entrada, partida };
+enum Tipo { chegada, partida };
 
 struct Evento {
-	Tipo tipo; double t; unsigned r;
+	Tipo tipo; 
+	double t; 
+	unsigned r;
+
+	Evento(Tipo tipo, double t, unsigned r) : tipo(tipo), t(t), r(r) {};
+	bool operator<(const Evento& that) const { return this->t > that.t; };
+	bool operator>(const Evento& that) const { return this->t < that.t; };
 };
